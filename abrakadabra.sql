b650d1ea4f50556a4ccd3d290dda622b2abb5171
itsnotTalha <talhajubayer737@gmail.com>
2025-11-23 12:31:24 +0600

    First Relation

---
+++ abrakadabra.sql
@@ -0,0 +1,19 @@
+USE new_schema;
+/*
+CREATE TABLE Customers(
+  Customer_id INT PRIMARY KEY,
+  first_name VARCHAR(45),
+  last_name VARCHAR(45),
+  age INT,
+  phone VARCHAR(13)
+);
+*
+INSERT INTO Customers(Customer_id, first_name, last_name, age, phone)
+VALUES
+(1, 'Talha', 'Jubayer', 12, '01001001010'),
+(2, 'Talha', 'Jubayer', 12, '01001001010'),
+(3, 'Talha', 'Jubayer', 12, '01001001010'),
+(4, 'Talha', 'Jubayer', 12, '01001001010'),
+(5, 'Talha', 'Jubayer', 12, '01001001010');
+*/
+SELECT * FROM Customers;
