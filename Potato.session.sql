28fb9a5a9f92d8d77e1e8a254caa7b36ed35b797
itsnotTalha <talhajubayer737@gmail.com>
2025-12-08 09:59:39 +0600

    Huddai disi

---
+++ Potato.session.sql
@@ -0,0 +1,33 @@
+-- use test_data1;
+
+-- CREATE TABLE program (
+-- short_name VARCHAR(8),
+-- full_name VARCHAR(100) NOT NULL,
+-- students_count INT,
+-- CONSTRAINT pk_program PRIMARY KEY(short_name)
+-- );
+
+-- INSERT INTO program VALUES
+-- ('CSE', 'Computer Engineerin', 450),
+-- ('EEE', 'Electrical', 550),
+-- ('BBA', 'Business', 400),
+-- ('ENG', 'English', 200); 
+/*
+CREATE TABLE table_name (
+    column_name datatype constraints
+);
+ALTER TABLE table_name action;
+
+ALTER TABLE table_name
+ADD COLUMN column_name datatype;
+
+PRIMARY KEY (column1, column2);
+
+
+SELECT * FROM program;
+*/
+
+-- use new_db;
+-- SELECT * FROM course_student;
+
+use test_data1;
