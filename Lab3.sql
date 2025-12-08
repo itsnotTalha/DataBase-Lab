31875ae8cff675bcfada98a9c9a5b516a756ccfc
itsnotTalha <talhajubayer737@gmail.com>
2025-12-08 06:53:02 +0600

    DBMS

---
+++ Lab3.sql
@@ -0,0 +1,14 @@
+-- -- USE new_db;
+-- -- SELECT * FROM course_student WHERE course_code=101;
+-- use new_db;
+-- CREATE TABLE employee(
+--     employee_id INT PRIMARY KEY AUTO_INCREMENT,
+--     first_name Text NOT NULL,
+--     last_name Text NOT NULL,
+--     gender Text,
+--     dept Text,
+--     salary INT,
+--     joinDate Text
+-- );
+
+
